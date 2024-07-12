32c65ac5a64363f743a0b3df7f4b9f1188d6a17a
adityasaivit <113006142+adityasaivit@users.noreply.github.com>
2024-07-12 17:51:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+count=0
+function increment(){
+    count=count+1
+    document.querySelector(".count").innerText=count
+}
+
+
+function reset(){
+    count=0
+    document.querySelector(".count").innerText=count
+}
