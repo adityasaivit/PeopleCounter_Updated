ad294cc731ba731c96ed341e85161d623bd3ad0d
adityasaivit <113006142+adityasaivit@users.noreply.github.com>
2024-07-13 11:06:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,4 +8,14 @@ function increment(){
 function reset(){
     count=0
     document.querySelector(".count").innerText=count
+    document.querySelector(".history p").innerText='Previous-entries:';
+}
+
+function save()
+{
+    console.log(count)
+    values=document.querySelector(".history p").innerText
+    document.querySelector(".history p").innerText=values+count+'-';
+
+
 }
